fe40384b0caa8b0b2c943846e23a2e57dd7a5133
xsfcl174 <xsfcl1742163.com>
2017-10-17 19:09:34 +0800

    提交测试

---
+++ src/main/java/com/fancl/factory/Test.java
@@ -0,0 +1,12 @@
+package com.fancl.factory;
+
+/**
+ * 作者 fanchengliang
+ * 日期 2017/10/17
+ * 项目 DesignPatternStudy
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
